9d52610f0f9f03730e24585164bac0866cd7cde5
Wayne Peters <w@ubuntu>
2019-12-05 07:54:40 -0800

    Fixed cellular modem operation

--- version1.1/etc/systemd/system/quectel-service.sh
+++ version1.1/etc/systemd/system/quectel-service.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-sleep 5
-#!/bin/bash
-
-if [ ! -f /etc/apn.conf ]; then
-  echo "/etc/apn.conf not found. Waiting for user configuration..."
-  while [ ! -f /etc/apn.conf ]; do
-      sleep 60
-  done
-  echo "APN is now configured..."
-fi
 
 echo "Starting quectel service for APN $(cat /etc/apn.conf)"
 quectel-CM -s "$(cat /etc/apn.conf)"
-
--- version1.1/opt/avnet-iot/services/quectel-service.sh
+++ version1.1/opt/avnet-iot/services/quectel-service.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-sleep 5
-
-if [ ! -f /etc/apn.conf ]; then
-  echo "/etc/apn.conf not found. Waiting for user configuration..."
-  while [ ! -f /etc/apn.conf ]; do
-      sleep 30
-  done
-  echo "APN is now configured..."
-fi
 
 echo "Starting quectel service for APN $(cat /etc/apn.conf)"
-while true; do
-    quectel-CM -s "$(cat /etc/apn.conf)"
-    sleep 30
-done &
+quectel-CM -s "$(cat /etc/apn.conf)"
